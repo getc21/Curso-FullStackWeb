c89996a30499315337aa1f8a8cfbc82e6d2ad039
getc2 <getc2@192.168.1.14>
2019-10-30 10:53:16 -0400

    Clase 9

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/', function () {
+    return '<h1>Hola mundo con laravel</h1>';
+});
+
+Route::get('/pruebas/{nombre?}', function ($nombre=null) {
+    $texto = '<h2>Texto desde una ruta</h2>';
+    $texto .= 'Nombre: '.$nombre;
+    
+    return view('pruebas',array(
+        'texto' => $texto
+    ));
+});
+
+Route::get('/animales', 'PruebasController@index');
